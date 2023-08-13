7c2e9b402491c7ef513a624cee25a5a9360a2429
Ludovic Roland <ludovic.c.roland@gmail.com>
2023-08-13 23:53:01 +0200

    chore: Add minimal configuration for the server

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.3")
+    implementation("io.ktor:ktor-server-netty:2.3.3")
+
     testImplementation(kotlin("test"))
 }
 
